5cc8c67ab15b70321ac2dcf01a2aa0c14779570d
Derek Nunes <dereknunes28@gmail.com>
2022-10-03 23:38:20 -0700

    startPage: validate value for player name entered before going to next page and shell code for placeShip

--- src/DOM/placeShip.js
+++ src/DOM/placeShip.js
@@ -1,1 +1,33 @@
 // second screen after player enters name, will be where user places ships in desired place
+
+const placeShipContent = () => {
+    // page content wrapper
+    const hWrapper = newDiv('','psWrapper');
+
+    
+    // append all content to wrapper
+    hWrapper.append();
+
+    return psWrapper;
+}
+
+export default placeShipContent;
+
+
+function newDiv(classname, idname) {
+    const element = document.createElement('div');
+    if (classname !== '') {
+        element.classList.add(`${classname}`);
+    }
+    if (idname !== '') {
+        element.setAttribute('id', `${idname}`);
+    }
+    return element;
+}
+
+function newButton(classname, idname ) {
+    const element = document.createElement('button');
+    element.classList.add(`${classname}`);
+    element.setAttribute('id',`${idname}`);
+    return element;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 import './style.css';
 import startPageContent from './DOM/startPage';
+import placeShipContent from './DOM/placeShip';
+
+// page content shell
+const pageContent = document.body;
 
 // start page content
 function startPage() {
@@ -9,4 +13,29 @@ function startPage() {
 	return element;
 }
 
-document.body.append(startPage());
+pageContent.append(startPage());
+
+// place ship content
+function placeShip() {
+	const element = placeShipContent();
+
+	return element;
+}
+
+
+
+
+
+// page gameflow
+document.addEventListener('click', function(e) {
+
+	if (e.target.matches('#hPlayButton')) {
+		const hPlayerNameInput = document.querySelector('#playerInfo');
+		if (hPlayerNameInput.value.length < 2 || hPlayerNameInput.value.length > 10) {
+			return
+		} else {
+			pageContent.innerHTML = '';
+			pageContent.append(placeShip());
+		}
+	}
+})
